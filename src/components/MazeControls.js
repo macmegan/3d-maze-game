8506fa4ee2fde9f94e3ef5378a83451e6f95b7e7
macmegan <50040678+macmegan@users.noreply.github.com>
2024-08-07 03:11:40 -0400

    resolve build error

--- src/components/MazeControls.js
+++ src/components/MazeControls.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import * as THREE from 'three'; 
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'; 
 import { snapToGrid, checkCollision } from '../utils/MazeUtils';
